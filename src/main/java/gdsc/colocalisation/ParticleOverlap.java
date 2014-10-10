ed38e6beecd5df10ae690a9e10bac7c6df15a66c
Alex Herbert <a.herbert@sussex.ac.uk>
2014-10-10 14:15:57 +0100

    Added a plugin to allow the particle overlap between two masks to be calculated.

---
+++ src/main/java/gdsc/colocalisation/ParticleOverlap.java
@@ -0,0 +1,239 @@
+package gdsc.colocalisation;
+
+/*----------------------------------------------------------------------------- 
+ * GDSC Plugins for ImageJ
+ * 
+ * Copyright (C) 2011 Alex Herbert
+ * Genome Damage and Stability Centre
+ * University of Sussex, UK
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *---------------------------------------------------------------------------*/
+
+import gdsc.utils.ImageJHelper;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.WindowManager;
+import ij.gui.GenericDialog;
+import ij.plugin.PlugIn;
+import ij.process.ImageProcessor;
+import ij.text.TextWindow;
+
+/**
+ * For all particles in a mask (defined by their unique pixel value), count the overlap with a second mask image. An
+ * intensity image is used to calculate the Mander's coefficient of each particle.
+ */
+public class ParticleOverlap implements PlugIn
+{
+	private static String FRAME_TITLE = "Particle Overlap";
+
+	private static String maskTitle1 = "";
+	private static String imageTitle = "";
+	private static String maskTitle2 = "";
+	private static boolean showTotal = false;
+
+	private static TextWindow tw = null;
+
+	private ImagePlus mask1Imp, imageImp, mask2Imp;
+
+	public void run(String arg)
+	{
+		if (WindowManager.getImageCount() == 0)
+		{
+			IJ.showMessage(FRAME_TITLE, "No images opened.");
+			return;
+		}
+
+		if (!showDialog())
+			return;
+
+		analyse();
+	}
+
+	private boolean showDialog()
+	{
+		GenericDialog gd = new GenericDialog(FRAME_TITLE);
+
+		gd.addMessage("For each particle in a mask (defined by unique pixel value)\n"
+				+ "count the overlap and Mander's coefficient with a second mask image");
+
+		String[] imageList = ImageJHelper.getImageList(ImageJHelper.GREY_SCALE, null);
+		String[] maskList = ImageJHelper.getImageList(ImageJHelper.GREY_8_16, null);
+
+		gd.addChoice("Particle_mask", maskList, maskTitle1);
+		gd.addChoice("Particle_image", imageList, imageTitle);
+		gd.addChoice("Overlap_mask", maskList, maskTitle2);
+		gd.addCheckbox("Show_total", showTotal);
+		gd.showDialog();
+
+		if (gd.wasCanceled())
+			return false;
+
+		maskTitle1 = gd.getNextChoice();
+		imageTitle = gd.getNextChoice();
+		maskTitle2 = gd.getNextChoice();
+		showTotal = gd.getNextBoolean();
+
+		mask1Imp = WindowManager.getImage(maskTitle1);
+		imageImp = WindowManager.getImage(imageTitle);
+		mask2Imp = WindowManager.getImage(maskTitle2);
+
+		if (mask1Imp == null)
+		{
+			IJ.error(FRAME_TITLE, "No particle mask specified");
+			return false;
+		}
+		if (!checkDimensions(mask1Imp, imageImp))
+		{
+			IJ.error(FRAME_TITLE, "Particle image must match the dimensions of the particle mask");
+			return false;
+		}
+		if (!checkDimensions(mask1Imp, mask2Imp))
+		{
+			IJ.error(FRAME_TITLE, "Overlap mask must match the dimensions of the particle mask");
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean checkDimensions(ImagePlus imp1, ImagePlus imp2)
+	{
+		if (imp2 == null)
+			return false;
+		int[] d1 = imp1.getDimensions();
+		int[] d2 = imp2.getDimensions();
+		// Just check width, height, depth 
+		for (int i : new int[] { 0, 1, 3 })
+			if (d1[i] != d2[i])
+				return false;
+		return true;
+	}
+
+	private void analyse()
+	{
+		// Dimensions are the same. Extract a stack for each image;
+		ImageStack m1 = extractStack(mask1Imp);
+		ImageStack m2 = extractStack(mask2Imp);
+		ImageStack i1 = extractStack(imageImp);
+
+		// Count the pixels in the particle mask
+		int[] n1 = new int[(mask1Imp.getBitDepth() == 8) ? 256 : 65536];
+		// Count the pixels in the particle mask that overlap
+		int[] no1 = new int[n1.length];
+		// Sum the pixels in the particle image
+		double[] s1 = new double[n1.length];
+		// Sum the pixels in the particle image that overlap
+		double[] so1 = new double[n1.length];
+
+		final int size = m1.getWidth() * m1.getHeight();
+		for (int n = 1; n <= m1.getSize(); n++)
+		{
+			ImageProcessor mask1 = m1.getProcessor(n);
+			ImageProcessor mask2 = m2.getProcessor(n);
+			ImageProcessor image1 = i1.getProcessor(n);
+
+			for (int i = 0; i < size; i++)
+			{
+				final int p1 = mask1.get(i);
+				if (p1 != 0)
+				{
+					final int p2 = mask2.get(i);
+					final float v1 = image1.getf(i);
+					n1[p1]++;
+					s1[p1] += v1;
+					if (p2 != 0)
+					{
+						no1[p1]++;
+						so1[p1] += v1;
+					}
+				}
+			}
+		}
+
+		// Summarise results
+		createResultsTable();
+
+		final String title = createTitle();
+		long sn1 = 0, sno1 = 0;
+		double ss1 = 0, sso1 = 0;
+		for (int id = 1; id < n1.length; id++)
+		{
+			if (n1[id] == 0)
+				continue;
+			sn1 += n1[id];
+			sno1 += no1[id];
+			ss1 += s1[id];
+			sso1 += so1[id];
+			addResult(title, id, n1[id], no1[id], s1[id], so1[id]);
+		}
+		if (showTotal)
+			addResult(title, 0, sn1, sno1, ss1, sso1);
+	}
+
+	private ImageStack extractStack(ImagePlus imp)
+	{
+		ImageStack oldStack = imp.getImageStack();
+		int channel = imp.getChannel();
+		int frame = imp.getFrame();
+		int size = imp.getNSlices();
+		ImageStack stack = new ImageStack(imp.getWidth(), imp.getHeight(), size);
+		for (int slice = 1; slice <= size; slice++)
+		{
+			int index = imp.getStackIndex(channel, slice, frame);
+			stack.setPixels(oldStack.getPixels(index), slice);
+		}
+		return stack;
+	}
+
+	private void createResultsTable()
+	{
+		if (tw == null || !tw.isShowing())
+		{
+			tw = new TextWindow(FRAME_TITLE + " Results", "Mask1\tImage1\tMask2\tID\tN\tNo\t%\tI\tIo\tMander's", "",
+					800, 500);
+		}
+	}
+
+	private String createTitle()
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append(getName(mask1Imp)).append("\t");
+		sb.append(getName(imageImp)).append("\t");
+		sb.append(getName(mask2Imp)).append("\t");
+		return sb.toString();
+	}
+
+	private String getName(ImagePlus imp)
+	{
+		String name = imp.getTitle();
+		int suffix = 0;
+		String[] prefix = { " [", "," };
+		if (imp.getNChannels() > 1)
+		{
+			name += prefix[suffix++] + "C=" + imp.getChannel();
+		}
+		if (imp.getNFrames() > 1)
+			name += prefix[suffix++] + "T=" + imp.getFrame();
+		if (suffix > 0)
+			name += "]";
+		return name;
+	}
+
+	private void addResult(String title, int id, long n1, long no1, double s1, double so1)
+	{
+		StringBuilder sb = new StringBuilder(title);
+		sb.append(id).append("\t");
+		sb.append(n1).append("\t");
+		sb.append(no1).append("\t");
+		sb.append(ImageJHelper.rounded((100.0 * no1) / n1)).append("\t");
+		sb.append(s1).append("\t");
+		sb.append(so1).append("\t");
+		sb.append(ImageJHelper.rounded(so1 / s1, 5)).append("\t");
+		tw.append(sb.toString());
+	}
+}
