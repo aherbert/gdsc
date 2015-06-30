17de8cac999292162faa53c8f9f708058bb84ecf
Alex Herbert <a.herbert@sussex.ac.uk>
2015-06-30 12:10:43 +0100

    Allow multiple instances of the FindFoci GUI

--- src/main/java/gdsc/foci/FindFociPlugin.java
+++ src/main/java/gdsc/foci/FindFociPlugin.java
@@ -21,6 +21,7 @@ import ij.IJ;
 import ij.ImageListener;
 import ij.ImagePlus;
 import ij.WindowManager;
+import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 
 import java.awt.event.WindowEvent;
@@ -35,12 +36,109 @@ import javax.swing.JFrame;
 /**
  * Provides a permanent form front-end for the FindFoci plugin filter
  */
-public class FindFociPlugin implements PlugIn, WindowListener, ImageListener, PropertyChangeListener
+public class FindFociPlugin implements PlugIn
 {
-	private static FindFociView instance;
-	private FindFociModel model;
-	private int currentChannel = 0;
-	private int currentFrame = 0;
+	private static FindFociView instance = null;
+
+	private class FindFociListener implements WindowListener, ImageListener, PropertyChangeListener
+	{
+		FindFociModel model;
+		FindFociView instance;
+		int currentChannel = 0;
+		int currentFrame = 0;
+
+		FindFociListener(FindFociModel model)
+		{
+			this.model = model;
+		}
+
+		public void addWindowListener(FindFociView instance)
+		{
+			this.instance = instance;
+			instance.addWindowListener(this);
+		}
+
+		public void windowOpened(WindowEvent e)
+		{
+		}
+
+		public void windowClosing(WindowEvent e)
+		{
+			WindowManager.removeWindow(instance);
+		}
+
+		public void windowClosed(WindowEvent e)
+		{
+		}
+
+		public void windowIconified(WindowEvent e)
+		{
+		}
+
+		public void windowDeiconified(WindowEvent e)
+		{
+		}
+
+		public void windowActivated(WindowEvent e)
+		{
+		}
+
+		public void windowDeactivated(WindowEvent e)
+		{
+		}
+
+		public void imageOpened(ImagePlus imp)
+		{
+			// Ignore
+		}
+
+		public void imageClosed(ImagePlus imp)
+		{
+			// Ignore
+		}
+
+		public void imageUpdated(ImagePlus imp)
+		{
+			if (imp == null)
+				return;
+
+			// Check if the image is the selected image in the model.
+			// If the slice has changed then invalidate the model
+			if (imp.getTitle().equals(model.getSelectedImage()))
+			{
+				int oldCurrentChannel = currentChannel;
+				int oldCurrentFrame = currentFrame;
+				getCurrentSlice();
+				if (oldCurrentChannel != currentChannel || oldCurrentFrame != currentFrame)
+				{
+					model.invalidate();
+				}
+			}
+		}
+
+		private void getCurrentSlice()
+		{
+			ImagePlus imp = WindowManager.getImage(model.getSelectedImage());
+			if (imp != null)
+			{
+				currentChannel = imp.getChannel();
+				currentFrame = imp.getFrame();
+			}
+			else
+			{
+				currentChannel = currentFrame = 0;
+			}
+		}
+
+		public void propertyChange(PropertyChangeEvent evt)
+		{
+			// Store the slice for the image when it changes.
+			if (evt.getPropertyName().equals("selectedImage"))
+			{
+				getCurrentSlice();
+			}
+		}
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -57,17 +155,33 @@ public class FindFociPlugin implements PlugIn, WindowListener, ImageListener, Pr
 
 		if (instance != null)
 		{
-			showInstance();
+			if (instance.isVisible())
+			{
+				// Ask if the user would like a second instance
+				GenericDialog gd = new GenericDialog(FindFoci.FRAME_TITLE);
+				gd.enableYesNoCancel();
+				gd.addMessage(FindFoci.FRAME_TITLE + " is already open.\n \nDo you want to create another instance?");
+				gd.showDialog();
+				if (gd.wasCanceled())
+					return;
+				if (gd.wasOKed())
+				{
+					showNewInstance();
+					return;
+				}
+			}
+			showInstance(instance);
 			return;
 		}
 
-		model = new FindFociModel();
+		FindFociModel model = new FindFociModel();
 		model.setResultsDirectory(System.getProperty("java.io.tmpdir"));
 		FindFociController controller = new ImageJController(model);
+		FindFociListener listener = new FindFociListener(model);
 
 		// Track when the image changes to a new slice
-		ImagePlus.addImageListener(this);
-		model.addPropertyChangeListener("selectedImage", this);
+		ImagePlus.addImageListener(listener);
+		model.addPropertyChangeListener("selectedImage", listener);
 
 		IJ.showStatus("Initialising FindFoci ...");
 
@@ -80,12 +194,12 @@ public class FindFociPlugin implements PlugIn, WindowListener, ImageListener, Pr
 
 			// it exists on the classpath
 			instance = new FindFociView(model, controller);
-			instance.addWindowListener(this);
+			listener.addWindowListener(instance);
 			instance.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 
 			IJ.register(FindFociView.class);
 
-			showInstance();
+			showInstance(instance);
 			IJ.showStatus("FindFoci ready");
 		}
 		catch (ExceptionInInitializerError e)
@@ -123,91 +237,28 @@ public class FindFociPlugin implements PlugIn, WindowListener, ImageListener, Pr
 		}
 	}
 
-	private void showInstance()
+	private void showNewInstance()
+	{
+		FindFociModel model = new FindFociModel();
+		model.setResultsDirectory(System.getProperty("java.io.tmpdir"));
+		FindFociController controller = new ImageJController(model);
+		FindFociListener listener = new FindFociListener(model);
+
+		// Track when the image changes to a new slice
+		ImagePlus.addImageListener(listener);
+		model.addPropertyChangeListener("selectedImage", listener);
+
+		FindFociView instance = new FindFociView(model, controller);
+		listener.addWindowListener(instance);
+		instance.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		showInstance(instance);
+	}
+
+	private void showInstance(FindFociView instance)
 	{
 		WindowManager.addWindow(instance);
 		instance.setVisible(true);
 		instance.toFront();
-	}
-
-	public void windowOpened(WindowEvent e)
-	{
-	}
-
-	public void windowClosing(WindowEvent e)
-	{
-		WindowManager.removeWindow(instance);
-	}
-
-	public void windowClosed(WindowEvent e)
-	{
-	}
-
-	public void windowIconified(WindowEvent e)
-	{
-	}
-
-	public void windowDeiconified(WindowEvent e)
-	{
-	}
-
-	public void windowActivated(WindowEvent e)
-	{
-	}
-
-	public void windowDeactivated(WindowEvent e)
-	{
-	}
-
-	public void imageOpened(ImagePlus imp)
-	{
-		// Ignore
-	}
-
-	public void imageClosed(ImagePlus imp)
-	{
-		// Ignore
-	}
-
-	public void imageUpdated(ImagePlus imp)
-	{
-		if (imp == null)
-			return;
-
-		// Check if the image is the selected image in the model.
-		// If the slice has changed then invalidate the model
-		if (imp.getTitle().equals(model.getSelectedImage()))
-		{
-			int oldCurrentChannel = currentChannel;
-			int oldCurrentFrame = currentFrame;
-			getCurrentSlice();
-			if (oldCurrentChannel != currentChannel || oldCurrentFrame != currentFrame)
-			{
-				model.invalidate();
-			}
-		}
-	}
-
-	private void getCurrentSlice()
-	{
-		ImagePlus imp = WindowManager.getImage(model.getSelectedImage());
-		if (imp != null)
-		{
-			currentChannel = imp.getChannel();
-			currentFrame = imp.getFrame();
-		}
-		else
-		{
-			currentChannel = currentFrame = 0;
-		}
-	}
-
-	public void propertyChange(PropertyChangeEvent evt)
-	{
-		// Store the slice for the image when it changes.
-		if (evt.getPropertyName().equals("selectedImage"))
-		{
-			getCurrentSlice();
-		}
 	}
 }
