7be9bc2550bd3ff70b1e11c0d381ddb2475cd225
aherbert <a.herbert@sussex.ac.uk>
2019-01-11 16:31:59 +0000

    Changed error message when no optimiser results

--- src/main/java/uk/ac/sussex/gdsc/foci/FindFociOptimiser_PlugIn.java
+++ src/main/java/uk/ac/sussex/gdsc/foci/FindFociOptimiser_PlugIn.java
@@ -79,9 +79,7 @@ import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.nio.file.Files;
@@ -389,7 +387,7 @@ public class FindFociOptimiser_PlugIn implements PlugIn {
       }
 
       if (result == null) {
-        IJ.error(TITLE, "No ROI points fall inside the mask image");
+        IJ.error(TITLE, "No results");
         return;
       }
 
