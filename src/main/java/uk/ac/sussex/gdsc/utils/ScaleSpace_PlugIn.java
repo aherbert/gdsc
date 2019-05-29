0f7fe0714d0d273c4552e3c15001bdcd720a1854
aherbert <a.herbert@sussex.ac.uk>
2019-05-29 16:40:23 +0100

    Add Laplacian of Gaussian option

--- src/main/java/uk/ac/sussex/gdsc/utils/ScaleSpace_PlugIn.java
+++ src/main/java/uk/ac/sussex/gdsc/utils/ScaleSpace_PlugIn.java
@@ -26,19 +26,35 @@ package uk.ac.sussex.gdsc.utils;
 
 import uk.ac.sussex.gdsc.UsageTracker;
 import uk.ac.sussex.gdsc.core.ij.ImageJUtils;
+import uk.ac.sussex.gdsc.core.ij.gui.ExtendedGenericDialog;
 import uk.ac.sussex.gdsc.core.utils.MathUtils;
+import uk.ac.sussex.gdsc.core.utils.TurboList;
+import uk.ac.sussex.gdsc.core.utils.concurrent.ConcurrencyUtils;
 
-import gnu.trove.list.array.TDoubleArrayList;
+import com.google.common.util.concurrent.Futures;
 
 import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
-import ij.gui.GenericDialog;
+import ij.Prefs;
 import ij.plugin.filter.GaussianBlur;
 import ij.plugin.filter.PlugInFilter;
+import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 
+import imagescience.feature.Differentiator;
+import imagescience.image.FloatImage;
+import imagescience.image.Image;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
+import java.util.function.UnaryOperator;
 
 /**
  * Create a scale-space representation of an image using a 2D Gaussian filter at different scales.
@@ -51,7 +67,10 @@ public class ScaleSpace_PlugIn implements PlugInFilter {
   /** The flags specifying the capabilities and needs. */
   private static final int FLAGS = DOES_ALL | NO_CHANGES;
 
-  /** The flags specifying the capabilities and needs for Difference of Gaussians. */
+  /**
+   * The flags specifying the capabilities and needs for a gradient image (Difference of Gaussians
+   * or Laplacian of Gaussian).
+   */
   private static final int DOG_FLAGS = DOES_8G | DOES_16 | DOES_32 | NO_CHANGES;
 
   private ImagePlus imp;
@@ -63,13 +82,19 @@ public class ScaleSpace_PlugIn implements PlugInFilter {
    * Contains the settings that are the re-usable state of the plugin.
    */
   private static class Settings {
+    private static final String[] OUTPUT_OPTIONS =
+        {"Gaussian", "Difference of Gaussians", "Laplacian of Gaussian"};
+    private static final int OPT_GAUSS = 0;
+    private static final int OPT_DOG = 1;
+    private static final int OPT_LOG = 2;
+
     /** The last settings used by the plugin. This should be updated after plugin execution. */
     private static final AtomicReference<Settings> lastSettings =
         new AtomicReference<>(new Settings());
 
     double minScale;
     double maxScale;
-    boolean outputDifferenceImage;
+    int outputOption;
 
     /**
      * Default constructor.
@@ -86,7 +111,7 @@ public class ScaleSpace_PlugIn implements PlugInFilter {
     private Settings(Settings source) {
       minScale = source.minScale;
       maxScale = source.maxScale;
-      outputDifferenceImage = source.outputDifferenceImage;
+      outputOption = source.outputOption;
     }
 
     /**
@@ -125,12 +150,12 @@ public class ScaleSpace_PlugIn implements PlugInFilter {
   private int showDialog() {
     settings = Settings.load();
 
-    final GenericDialog gd = new GenericDialog(TITLE);
+    final ExtendedGenericDialog gd = new ExtendedGenericDialog(TITLE);
     gd.addMessage("Computes a scale-space representation of an image.\n"
         + "All scales up to the minimum image dimension in X or Y are computed.");
     gd.addNumericField("Min_scale", settings.minScale, 2);
     gd.addNumericField("Max_scale", settings.maxScale, 2);
-    gd.addCheckbox("Difference_of_Gaussians", settings.outputDifferenceImage);
+    gd.addChoice("Output", Settings.OUTPUT_OPTIONS, settings.outputOption);
     gd.addHelp(uk.ac.sussex.gdsc.help.UrlUtils.UTILITY);
     gd.showDialog();
     settings.save();
@@ -139,74 +164,143 @@ public class ScaleSpace_PlugIn implements PlugInFilter {
     }
     settings.minScale = gd.getNextNumber();
     settings.maxScale = gd.getNextNumber();
-    settings.outputDifferenceImage = gd.getNextBoolean();
+    settings.outputOption = gd.getNextChoiceIndex();
 
     if (gd.invalidNumber()) {
       IJ.error(TITLE, "Bad input number");
       return DONE;
     }
 
-    return (settings.outputDifferenceImage) ? DOG_FLAGS : FLAGS;
+    return (settings.outputOption == Settings.OPT_GAUSS) ? FLAGS : DOG_FLAGS;
   }
 
   @Override
   public void run(ImageProcessor ip) {
     final int width = ip.getWidth();
     final int height = ip.getHeight();
-    ImageStack stack = new ImageStack(width, height);
-    stack.addSlice("t=0", ip);
+    final ImageStack stack = new ImageStack(width, height);
 
-    // Limits on the scale:
+    if (settings.outputOption == Settings.OPT_GAUSS) {
+      stack.addSlice("t=0", ip);
+    }
+
+    // Default limits on the scale:
     // A Gaussian blur below 0.5 in a single dimension, or
     // a Gaussian blur with a width above the dimension size is meaningless.
-    final double limitT = MathUtils.pow2(Math.min(width, height));
+    // For a more reasonable upper limit use 1/4 of the dimension size to avoid very large
+    // Gaussian kernels.
+    final double limitT = MathUtils.pow2(Math.min(width, height) / 4);
     double minT = MathUtils.clip(0.25, limitT, settings.minScale);
     // If max is 0 use the computed max for auto-range
     double maxT =
         MathUtils.clip(minT, limitT, (settings.maxScale > 0 ? settings.maxScale : limitT));
 
-    // This could be multi-threaded using Callable<?>.
-    // The returned results would have to be sorted by t.
+    // Multi-thread
+    ExecutorService executor = Executors.newFixedThreadPool(Prefs.getThreads());
+    List<Future<Pair<Double, ImageProcessor>>> futures = new TurboList<>();
 
-    // Blur up to the minimum image dimension (above which the Gaussian blur is meaningless)
-    final GaussianBlur gb = new GaussianBlur();
-    gb.showProgress(false);
-    final TDoubleArrayList scales = new TDoubleArrayList();
+    BiFunction<ImageProcessor, Double, ImageProcessor> fun = createFunction();
+
     for (double scaleT = minT; scaleT <= maxT; scaleT *= 2) {
-      scales.add(scaleT);
-      // Gaussian blur. Since it is 2D use sqrt(t) for each dimension
-      final double sigma = Math.sqrt(scaleT);
-      ImageProcessor scaledIp = ip.duplicate();
-      if (settings.outputDifferenceImage) {
-        // Float-image required for correct difference image
-        scaledIp = scaledIp.toFloat(0, null);
-      }
-      gb.blurGaussian(scaledIp, sigma);
-      stack.addSlice("t=" + MathUtils.rounded(scaleT), scaledIp);
+      final Double t = scaleT;
+      futures.add(executor.submit(() -> {
+        // Since it is 2D use sqrt(t) for each dimension
+        final Double sigma = Math.sqrt(t);
+        final ImageProcessor scaledIp = fun.apply(ip, sigma);
+        return Pair.of(t, scaledIp);
+      }));
     }
 
+    ConcurrencyUtils.waitForCompletionUncheckedT(futures);
+
+    // Sort by scale and add to the stack.
+    // Note the futures are all complete so get the result unchecked.
+    futures.stream().map(Futures::getUnchecked)
+        .sorted((r1, r2) -> Double.compare(r1.getLeft(), r2.getLeft())).forEachOrdered(
+            pair -> stack.addSlice("t=" + MathUtils.rounded(pair.getLeft()), pair.getRight()));
+
     String suffix;
-    if (settings.outputDifferenceImage && stack.getSize() > 2) {
+    ImageStack outputStack;
+    if (settings.outputOption == Settings.OPT_DOG && stack.getSize() > 1) {
       suffix = "Difference of Gaussians";
-      final ImageStack diffStack = new ImageStack(width, height);
-      // Ignore first blur minus no blur
-      float[] pixels2 = (float[]) stack.getPixels(2);
-      for (int slice = 3; slice <= stack.size(); slice++) {
+      outputStack = new ImageStack(width, height);
+      float[] pixels2 = (float[]) stack.getPixels(1);
+      for (int slice = 2; slice <= stack.size(); slice++) {
         final float[] pixels1 = pixels2;
         pixels2 = (float[]) stack.getPixels(slice);
-        // Scale normalised using the smaller scale
+        // Q. Does this need to be scale normalised?
         for (int i = 0; i < pixels1.length; i++) {
           final float v = pixels1[i] - pixels2[i];
           pixels1[i] = v;
         }
-        diffStack.addSlice(stack.getSliceLabel(slice) + "-" + stack.getSliceLabel(slice - 1),
+        outputStack.addSlice(stack.getSliceLabel(slice) + "-" + stack.getSliceLabel(slice - 1),
             pixels1);
       }
-      stack = diffStack;
+    } else if (settings.outputOption == Settings.OPT_LOG) {
+      suffix = "Laplacian of Gaussian";
+      outputStack = stack;
     } else {
       suffix = TITLE;
+      outputStack = stack;
     }
 
-    ImageJUtils.display(imp.getTitle() + " " + suffix, stack);
+    ImageJUtils.display(imp.getTitle() + " " + suffix, outputStack);
+  }
+
+  /**
+   * Creates the function to processor the input image processor.
+   *
+   * @return the function
+   */
+  private BiFunction<ImageProcessor, Double, ImageProcessor> createFunction() {
+    final UnaryOperator<ImageProcessor> converter = createConverter();
+
+    if (settings.outputOption == Settings.OPT_LOG) {
+      return (ip, sigma) -> {
+        FloatProcessor fp = (FloatProcessor) converter.apply(ip);
+        final FloatImage img = (FloatImage) Image.wrap(new ImagePlus(null, fp));
+        final Differentiator differentiator = new Differentiator();
+        final Image Ixx = differentiator.run(img.duplicate(), sigma, 2, 0, 0);
+        final Image Iyy = differentiator.run(img, sigma, 0, 2, 0);
+        Iyy.add(Ixx);
+        fp = (FloatProcessor) Iyy.imageplus().getProcessor();
+
+        // Post-processing for scale normalisation and negation.
+        // This makes the appearance match the Difference of Gaussians (but the scale
+        // make be different).
+        double norm = -sigma * sigma;
+        final float[] data = (float[]) fp.getPixels();
+        for (int i = 0; i < data.length; i++) {
+          data[i] *= norm;
+        }
+
+        return fp;
+      };
+    }
+
+    // Others use a Gaussian blur
+    return (ip, sigma) -> {
+      final GaussianBlur gb = new GaussianBlur();
+      gb.showProgress(false);
+      ip = converter.apply(ip);
+      gb.blurGaussian(ip, sigma);
+      return ip;
+    };
+  }
+
+  /**
+   * Creates the converter to prepare the input image processor.
+   *
+   * @return the converter
+   */
+  private UnaryOperator<ImageProcessor> createConverter() {
+    // Duplicate the processor.
+    if (settings.outputOption == Settings.OPT_GAUSS) {
+      return ImageProcessor::duplicate;
+    }
+    // Float-image required for correct DoG or LoG
+    return ip -> {
+      return (ip instanceof FloatProcessor) ? (FloatProcessor) ip.duplicate() : ip.toFloat(0, null);
+    };
   }
 }
