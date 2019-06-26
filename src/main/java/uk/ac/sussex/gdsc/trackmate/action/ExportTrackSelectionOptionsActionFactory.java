d8cbc2ef4a4fac24863d12048a7946428f54145a
aherbert <a.herbert@sussex.ac.uk>
2019-06-26 11:05:05 +0100

    Fix info text

--- src/main/java/uk/ac/sussex/gdsc/trackmate/action/ExportTrackSelectionOptionsActionFactory.java
+++ src/main/java/uk/ac/sussex/gdsc/trackmate/action/ExportTrackSelectionOptionsActionFactory.java
@@ -39,7 +39,7 @@ import javax.swing.ImageIcon;
 public class ExportTrackSelectionOptionsActionFactory implements TrackMateActionFactory {
   /** Description of the action. */
   private static final String INFO_TEXT =
-      "<html><p>This action allows configuration of the export track selection action.</p></html>";
+      "<html><p>This action configures the data exported by the export track selection action.</p></html>";
 
   /** Key used for the action. */
   private static final String KEY = "EXPORT_TRACK_SELECTION_OPTIONS";
