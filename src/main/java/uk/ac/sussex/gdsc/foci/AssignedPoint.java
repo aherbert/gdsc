85f1421a77af33c1827793c4a8e94d9ca834ca19
aherbert <a.herbert@sussex.ac.uk>
2021-05-21 17:16:22 +0100

    Add support for channel and frame

--- src/main/java/uk/ac/sussex/gdsc/foci/AssignedPoint.java
+++ src/main/java/uk/ac/sussex/gdsc/foci/AssignedPoint.java
@@ -25,7 +25,8 @@
 package uk.ac.sussex.gdsc.foci;
 
 /**
- * Stores a 2D/3D point with an assigned Id.
+ * Stores a 2D/3D point with an assigned Id, channel and frame. The point supports XYZCT positioning
+ * with additional identifiers for use in algorithms.
  */
 public class AssignedPoint extends BasePoint {
   /** The id. */
@@ -33,6 +34,29 @@ public class AssignedPoint extends BasePoint {
 
   /** The assigned id. */
   protected int assignedId;
+
+  /** The channel. */
+  protected int channel;
+
+  /** The frame. */
+  protected int frame;
+
+  /**
+   * Instantiates a new assigned point using XYZCT position.
+   *
+   * @param x the x
+   * @param y the y
+   * @param z the z
+   * @param channel the channel (c)
+   * @param frame the time frame (t)
+   * @param id the id
+   */
+  public AssignedPoint(int x, int y, int z, int channel, int frame, int id) {
+    super(x, y, z);
+    this.channel = channel;
+    this.frame = frame;
+    this.id = id;
+  }
 
   /**
    * Instantiates a new assigned point.
@@ -95,6 +119,42 @@ public class AssignedPoint extends BasePoint {
     return assignedId;
   }
 
+  /**
+   * Sets the point Channel.
+   *
+   * @param channel the new channel
+   */
+  public void setChannel(int channel) {
+    this.channel = channel;
+  }
+
+  /**
+   * Gets the channel.
+   *
+   * @return the channel
+   */
+  public int getChannel() {
+    return channel;
+  }
+
+  /**
+   * Sets the point Frame.
+   *
+   * @param frame the new frame
+   */
+  public void setFrame(int frame) {
+    this.frame = frame;
+  }
+
+  /**
+   * Gets the frame.
+   *
+   * @return the frame
+   */
+  public int getFrame() {
+    return frame;
+  }
+
   @Override
   public boolean equals(Object object) {
     if (this == object) {
@@ -109,15 +169,12 @@ public class AssignedPoint extends BasePoint {
     final AssignedPoint that = (AssignedPoint) object;
 
     return x == that.x && y == that.y && z == that.z && id == that.id
-        && assignedId == that.assignedId;
+        && assignedId == that.assignedId && channel == that.channel && frame == that.frame;
   }
 
   @Override
   public int hashCode() {
-    // Note: floatToRawIntBits does not unify all possible NaN values
-    // However since the equals() will fail for NaN values we are not
-    // breaking the java contract.
-    return (41 * (41 * (41 * (41 * (41 + Float.floatToRawIntBits(x)) + Float.floatToRawIntBits(y))
-        + Float.floatToRawIntBits(z)) + id) + assignedId);
+    return (41 * (41 * (41 * (41 * (41 * (41 * (41 + x) + y) + z) + id) + assignedId) + channel)
+        + frame);
   }
 }
